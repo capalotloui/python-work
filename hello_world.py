3855acbc5492b21e2dd34bd720a7d4a9cf720723
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:20:16 -0500

    assn 1

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
 print("hello world my name is ayenn")
+name="ayenn"
+age=15
+print("my name is",name,"i am",age, "years old")
+futureAge=16
+print("my name is",name,"and i will",futureAge,"years old")
---
+++ variables.py
@@ -0,0 +1,25 @@
+name="ayenn"
+age=15
+print("my name is","name","and i am",age)
+
+futureAge=16
+print("next year, i will be",futureAge,"years old")
+
+
+randomNum=1.0
+futureAge= randomNum + age
+
+print("next year i will be",futureAge)
+randomInt=5
+randomFloat=7.2
+randomBool= True
+randomNone= None
+randomString= "sup"
+
+print(type(randomInt))
+print(type(randomFloat))
+print(type(randomBool))
+print(type(randomNone))
+print(type(randomString))
+
+print("my name is", name , "and i am", str(age))
